7444f16f9e95b6c62504809757c5a8f82a2221dc
Margarita Morozova <s.volland@mail.ru>
2022-11-09 13:10:32 +0300

    Added tests

--- halfQSort/halfQSort/main.c
+++ halfQSort/halfQSort/main.c
@@ -2,6 +2,11 @@
 #include <stdlib.h>
 #include <stdbool.h>
 
+#define firstTestSize 10
+#define secondTestSize 5
+#define thirdTestSize 2
+#define fourthTestSize 4
+
 int* halfQSort(int* pointerToArray, int size)
 {
 	int index = 0;
@@ -33,8 +38,82 @@ int* randomFilling(int* pointerToArray, int size)
 	return pointerToArray;
 }
 
+bool firstTest()
+{
+	int testArray[firstTestSize] = { 10, 5, 7, 3, 9, 10, 45, 23, 7, 90 };
+	int rightArray[firstTestSize] = { 5, 7, 3, 9, 7, 10, 10, 45, 23, 90 };
+	bool isSortingRight = true;
+	halfQSort(testArray, firstTestSize);
+	for (int i = 0; i < firstTestSize; i++)
+	{
+		if (testArray[i] != rightArray[i])
+		{
+			isSortingRight = false;
+		}
+	}
+
+	return isSortingRight;
+}
+
+bool secondTest()
+{
+	int testArray[secondTestSize] = { 1, 5, 3, 6, 7};
+	int rightArray[secondTestSize] = { 1, 5, 3, 6, 7 };
+	bool isSortingRight = true;
+	halfQSort(testArray, secondTestSize);
+	for (int i = 0; i < secondTestSize; i++)
+	{
+		if (testArray[i] != rightArray[i])
+		{
+			isSortingRight = false;
+		}
+	}
+
+	return isSortingRight;
+}
+
+bool thirdTest()
+{
+	int testArray[thirdTestSize] = { 2, -5};
+	int rightArray[thirdTestSize] = {-5, 2};
+	bool isSortingRight = true;
+	halfQSort(testArray, thirdTestSize);
+	for (int i = 0; i < thirdTestSize; i++)
+	{
+		if (testArray[i] != rightArray[i])
+		{
+			isSortingRight = false;
+		}
+	}
+
+	return isSortingRight;
+}
+
+bool fourthTest()
+{
+	int testArray[fourthTestSize] = { 1, 1, 1, 1};
+	int rightArray[fourthTestSize] = { 1, 1, 1, 1};
+	bool isSortingRight = true;
+	halfQSort(testArray, fourthTestSize);
+	for (int i = 0; i < fourthTestSize; i++)
+	{
+		if (testArray[i] != rightArray[i])
+		{
+			isSortingRight = false;
+		}
+	}
+
+	return isSortingRight;
+}
+
 int main()
 {
+	if (!firstTest() || !secondTest() || !thirdTest() || !fourthTest())
+	{
+		printf("Tests failed :c");
+		return -1;
+	}
+
 	int size = 0;
 	printf("Enter the size of the array: ");
 	scanf_s("%d", &size);
@@ -53,12 +132,11 @@ int main()
 	}
 
 	halfQSort(firstArray, size);
-
 	printf("\nHalf-sorted array: ");
 	for (int i = 0; i < size; i++)
 	{
 		printf("%d ", firstArray[i]);
 	}
-
+	
 	return 0;
 }
