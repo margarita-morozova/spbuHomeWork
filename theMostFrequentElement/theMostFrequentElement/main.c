6b3afce22faf974056e3e863bdbc462566032ec1
Margarita Morozova <s.volland@mail.ru>
2022-11-13 02:56:06 +0300

    Added tests

--- theMostFrequentElement/theMostFrequentElement/main.c
+++ theMostFrequentElement/theMostFrequentElement/main.c
@@ -2,6 +2,9 @@
 #include <stdlib.h>
 #include <stdbool.h>
 
+#define firstTestArraySize 10
+#define secondTestArraySize 9
+
 void qSort(int* originalArray, int size, int beginning, int ending)
 {
 	int supportElement = beginning;
@@ -66,11 +69,58 @@ int* randomFilling(int* pointerToArray, int size)
 	return pointerToArray;
 }
 
+bool firstTest()
+{
+	int testArray[firstTestArraySize] = { 5, 5, 3, 4, 2, 8, 8, 9, 8, 0 };
+	int rightArray[firstTestArraySize] = { 0, 2, 3, 4, 5, 5, 8, 8, 8, 9 };
+	int answer = 8;
+	qSort(testArray, firstTestArraySize, 0, firstTestArraySize - 1);
+	for (int i = 0; i < firstTestArraySize; i++)
+	{
+		if (testArray[i] != rightArray[i])
+		{
+			return false;
+		}
+	}
+
+	return theMostFrequentElement(testArray, firstTestArraySize) == answer;
+}
+
+bool secondTest()
+{
+	int testArray[secondTestArraySize] = { 8, 8, 8, 5, 5, 5, 4, 0, 9 };
+	int rightArray[secondTestArraySize] = { 0, 4, 5, 5, 5, 8, 8, 8, 9 };
+	int answer = 5;
+	qSort(testArray, secondTestArraySize, 0, secondTestArraySize - 1);
+	for (int i = 0; i < secondTestArraySize; i++)
+	{
+		if (testArray[i] != rightArray[i])
+		{
+			return false;
+		}
+	}
+
+	return theMostFrequentElement(testArray, secondTestArraySize) == answer;
+}
+
 int main()
 {
+	if (!firstTest() || !secondTest())
+	{
+		printf("There are some mistakes in the programm :c\n");
+		return -1;
+	}
+
 	int size = 0;
 	printf("Enter the size of the array: ");
 	scanf_s("%d", &size);
+	while (size < 1)
+	{
+		printf("The size of the array has to be positive number\n");
+		printf("Enter the size of the array: ");
+		scanf_s("%d", &size);
+	}
+
 	int* firstArray = (int*)calloc(size, sizeof(int));
 	if (firstArray == NULL)
 	{
