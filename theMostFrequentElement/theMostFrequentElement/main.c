ab5df06af8ced9de0ca039371775db1800cc1aa5
Margarita Morozova <s.volland@mail.ru>
2022-11-13 01:14:15 +0300

    Created qsort and random filling functions

--- theMostFrequentElement/theMostFrequentElement/main.c
+++ theMostFrequentElement/theMostFrequentElement/main.c
@@ -1,8 +1,48 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+void qSort(int* originalArray, int size, int beginning, int ending)
+{
+	int supportElement = beginning;
+	for (int i = beginning + 1; i < ending + 1; i++)
+	{
+		if (originalArray[supportElement] > originalArray[i])
+		{
+			int temporaryPlace = originalArray[i];
+			for (int j = i; j >= supportElement; j--)
+			{
+				originalArray[j] = originalArray[j - 1];
+			}
+
+			originalArray[supportElement] = temporaryPlace;
+			++supportElement;
+		}
+	}
+
+	if (supportElement - beginning > 0)
+	{
+		qSort(originalArray, supportElement - beginning, beginning, supportElement - 1);
+	}
+
+	if (ending - supportElement > 0)
+	{
+		qSort(originalArray, ending - supportElement, supportElement + 1, ending);
+	}
+}
+
+int* randomFilling(int* pointerToArray, int size)
+{
+	for (int i = 0; i < size; i++)
+	{
+		pointerToArray[i] = rand() % 100;
+	}
+
+	return pointerToArray;
+}
 
 int main()
 {
-	printf("Hello world!");
+	
 
 	return 0;
 }
