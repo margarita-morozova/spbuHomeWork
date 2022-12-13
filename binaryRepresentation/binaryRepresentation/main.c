77d2ef012477ea5031bd9163b3935735517ebe16
Margarita Morozova <s.volland@mail.ru>
2022-12-14 01:47:47 +0300

    Added Convert to Binary and Print Binary functions

--- binaryRepresentation/binaryRepresentation/main.c
+++ binaryRepresentation/binaryRepresentation/main.c
@@ -1,12 +1,52 @@
 #include <stdio.h>
 #include <locale.h>
 #include <stdlib.h>
+#include <string.h>
+#include <math.h>
 
-int main()
-{
+#define INT_SIZE 8
+#define TWO_POWER_EIGHT 256;
+
+void convertToBin(int* binNumber, int number) {
+	int index = INT_SIZE - 1;
+	while (number > 0) {
+		binNumber[index] = number & 1;
+		index--;
+		number = number >> 1;
+	}
+}
+
+void printBin(int* number) {
+	for (int i = 0; i < INT_SIZE; i++) {
+		printf("%d", number[i]);
+	}
+}
+
+int main() {
 	setlocale(LC_ALL, "ru - RU");
 	system("chcp 1251");
-	printf("Привет!");
+	
+	int firstNumber = 0;
+	int secondNumber = 0;
+	printf("Введите два числа: ");
+	scanf_s("%d %d", &firstNumber, &secondNumber);
+	int* binFirstNumber = calloc(INT_SIZE, sizeof(int));
+	int* binSecondNumber = calloc(INT_SIZE, sizeof(int));
+	if (firstNumber < 0) {
+		firstNumber += TWO_POWER_EIGHT;
+	}
+
+	if (secondNumber < 0) {
+		secondNumber += TWO_POWER_EIGHT;
+	}
+
+	convertToBin(binFirstNumber, firstNumber);
+	convertToBin(binSecondNumber, secondNumber);
+	printf("\nYour first number is ");
+	printBin(binFirstNumber);
+	printf("\nYour second number is ");
+	printBin(binSecondNumber);
+
 
 	return 0;
 }
