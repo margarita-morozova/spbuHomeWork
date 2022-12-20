0c283177575b51af93682ef4ed0689d4a50cdf45
Margarita Morozova <s.volland@mail.ru>
2022-12-20 21:56:44 +0300

    Fixed Evaluate Expression function, added tests

--- advancedBracketBalance/advancedBracketBalance/main.c
+++ advancedBracketBalance/advancedBracketBalance/main.c
@@ -3,44 +3,67 @@
 #include <stdio.h>
 #include <malloc.h>
 
+#define FIRST_TEST_LENGTH 6
+#define SECOND_TEST_LENGTH 7
+
 int evaluateExpression(char expression[], int length, Stack* stack) {
     for (int i = 0; i < length; i++) {
         char element = expression[i];
-        printf("%c", element);
         if (element == '{' || element == '(' || element == '[') {
             push(stack, element);
         }
-        else if (element == ')' && !isEmpty(stack)) {
-            if (pop(stack) != '(') {
+        else if (element == '}' || element == ')' || element == ']') {
+            if (isEmpty(stack)) {
                 deleteStack(stack);
                 return -1;
             }
-        }
-        else if (element == '}' && !isEmpty(stack)) {
-            if (pop(stack) != '{') {
+
+            char previousElement = pop(stack);
+            if (previousElement == '(' && element != ')') {
                 deleteStack(stack);
                 return -1;
             }
-        }
-        else if (element == ']' && !isEmpty(stack)) {
-            if (pop(stack) != '[') {
+            else if (previousElement == '{' && element != '}') {
                 deleteStack(stack);
                 return -1;
             }
-        } 
-        else if (isEmpty(stack)) {
-            deleteStack(stack);
-            return -1;
+            else if (previousElement == '[' && element != ']') {
+                deleteStack(stack);
+                return -1;
+            }
         } else {
             deleteStack(stack);
             return -2;
         }
     }
 
+    if (!isEmpty(stack)) {
+        deleteStack(stack);
+        return -1;
+    }
+
+    deleteStack(stack);
     return 0;
 }
 
+bool rightBalanceTest() {
+    Stack* stack = createStack();
+    char expression[FIRST_TEST_LENGTH + 1] = "({[]})";
+    return (evaluateExpression(expression, FIRST_TEST_LENGTH, stack) == 0);
+}
+
+bool wrongBalanceTest() {
+    Stack* stack = createStack();
+    char expression[SECOND_TEST_LENGTH + 1] = "(())[]{";
+    return (evaluateExpression(expression, SECOND_TEST_LENGTH, stack) != 0);
+}
+
 int main() {
+    if (!rightBalanceTest() || !wrongBalanceTest()) {
+        printf("There are some problems in the programm :c");
+        return -1;
+    }
+
     int length = 0;
     Stack* stack = createStack();
     printf("Enter the number of brackets: ");
@@ -54,8 +77,7 @@ int main() {
     }
     else if (result == -2) {
         printf("Failed to identify the symbol");
-    } 
-    else {
+    } else {
         printf("Right bracket balance!");
     }
 
