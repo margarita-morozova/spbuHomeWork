023e26d89be3512a7ba25858e00e401f45678c79
Margarita Morozova <s.volland@mail.ru>
2022-12-20 18:52:00 +0300

    Created the files for the task Advanced Bracket Balance, added stack files

---
+++ advancedBracketBalance/advancedBracketBalance/main.c
@@ -0,0 +1,9 @@
+#include "stack.h"
+
+#include <stdio.h>
+
+int main() {
+	printf("Hello world!");
+
+	return 0;
+}
---
+++ advancedBracketBalance/advancedBracketBalance/stack.h
@@ -0,0 +1,22 @@
+#pragma once
+#include <stdbool.h>
+
+typedef struct Stack Stack;
+
+// add one element to stack
+int push(Stack* stack, int element);
+
+// take one element from stack
+int pop(Stack* stack);
+
+// is stack empty?
+bool isEmpty(Stack* stack);
+
+// create void stack
+Stack* createStack(void);
+
+// delete the whole stack
+void deleteStack(Stack* stack);
+
+// to look at the first element of stack
+int top(Stack* stack);
