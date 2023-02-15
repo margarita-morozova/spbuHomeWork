582daba734529bf29b15e58ee3a9fc044a08c6de
Margarita Morozova <s.volland@mail.ru>
2023-02-15 19:44:26 +0300

    fixed stack, input data, delited extra lines, exit function, messages to user

--- sortingStation/sortingStation/main.c
+++ sortingStation/sortingStation/main.c
@@ -3,11 +3,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
+#include <string.h>
 
+#define MAX_SIZE 101
 #define FIRST_TEST_SIZE 7
 #define SECOND_TEST_SIZE 3
 
-int sortingStation(char expression[], char answer[], int length, int *amountOfBrackets, Stack* stack) {
+int sortingStation(char expression[], char answer[]) {
+	int errorCode = 0;
+	int length = (int)strlen(expression);
+	int amountOfBrackets = 0;
+	Stack* stack = createStack(&errorCode);
+	if (errorCode != 0) {
+		return -2;
+	}
+
 	int indexOfAnswer = 0;
 	for (int i = 0; i < length; i++) {
 		char symbol = expression[i];
@@ -17,60 +27,83 @@ int sortingStation(char expression[], char answer[], int length, int *amountOfBr
 		}
 		else if (symbol == '(') {
 			push(stack, symbol);
-			* amountOfBrackets += 1;
+			++amountOfBrackets;
 		}
 		else if (symbol == ')' && !isEmpty(stack)) {
-			*amountOfBrackets += 1;
-			char topStack = pop(stack);
+			char topStack = pop(stack, &errorCode);
+			if (errorCode != 0) {
+				return -1;
+			}
+
 			while (topStack != '(' && !isEmpty(stack)) {
 				answer[indexOfAnswer] = topStack;
-				topStack = pop(stack);
-				indexOfAnswer++;
+				topStack = pop(stack, &errorCode);
+				if (errorCode != 0) {
+					return -1;
+				}
+
+				++indexOfAnswer;
+				++amountOfBrackets;
 			}
+
 			if (topStack != '(' && isEmpty(stack)) {
-				printf("There are missing bracket");
-				deleteStack(stack);
+				deleteStack(stack, &errorCode);
 				return -1;
 			}
 		}
 		else if (symbol == '+' || symbol == '-') {
-			while (!isEmpty(stack) && (top(stack) == '+' || top(stack) == '-' || top(stack) == '*' || top(stack) == '/')) {
-				answer[indexOfAnswer] = pop(stack);
+			while (!isEmpty(stack) && (top(stack, &errorCode) == '+' || top(stack, &errorCode) == '-' || top(stack, &errorCode) == '*' || top(stack, &errorCode) == '/')) {
+				if (errorCode != 0) {
+					return -1;
+				}
+
+				answer[indexOfAnswer] = pop(stack, &errorCode);
 				indexOfAnswer++;
 			}
 			push(stack, symbol);
 		}
 		else if (symbol == '*' || symbol == '/') {
-			while (!isEmpty(stack) && (top(stack) == '*' || top(stack) == '/')) {
-				answer[indexOfAnswer] = pop(stack);
+			while (!isEmpty(stack) && (top(stack, &errorCode) == '*' || top(stack, &errorCode) == '/')) {
+				if (errorCode != 0) {
+					return -1;
+				}
+				
+				answer[indexOfAnswer] = pop(stack, &errorCode);
+				if (errorCode != 0) {
+					return -1;
+				}
+
 				indexOfAnswer++;
 			}
 			push(stack, symbol);
 		} else {
-			printf("The expression is incorrect");
-			deleteStack(stack);
+			deleteStack(stack, &errorCode);
 			return -1;
 		}
 	}
 
-	if (indexOfAnswer < (length - *amountOfBrackets) && !isEmpty(stack)) {
-		for (indexOfAnswer; indexOfAnswer < length; indexOfAnswer++) {
-			answer[indexOfAnswer] = pop(stack);
+	if (indexOfAnswer < (length - amountOfBrackets) && !isEmpty(stack)) {
+		for (indexOfAnswer; indexOfAnswer < length - amountOfBrackets; ++indexOfAnswer) {
+			answer[indexOfAnswer] = pop(stack, &errorCode);
+			if (errorCode != 0) {
+				return -1;
+			}
 		}
 	} else {
 		return -1;
 	}
 
+	answer[length - amountOfBrackets] = '\0';
+	deleteStack(stack, &errorCode);
 	return 0;
 }
 
 bool firstTest() {
-	char expression[FIRST_TEST_SIZE] = "(1+1)*2";
-	Stack* stack = createStack();
-	char answer[FIRST_TEST_SIZE] = "0000000";
+	char expression[FIRST_TEST_SIZE + 1] = "(1+1)*2";
+	char answer[FIRST_TEST_SIZE + 1] = "";
 	char rightAnswer[FIRST_TEST_SIZE - 2] = "11+2*";
-	int amountOfBrackets = 0;
-	int result = sortingStation(expression, answer, FIRST_TEST_SIZE, &amountOfBrackets, stack);
+	int amountOfBrackets = 2;
+	int result = sortingStation(expression, answer);
 	for (int i = 0; i < FIRST_TEST_SIZE - amountOfBrackets; i++) {
 		if (answer[i] != rightAnswer[i]) {
 			return false;
@@ -81,13 +114,11 @@ bool firstTest() {
 }
 
 bool secondTest() {
-	char expression[SECOND_TEST_SIZE] = "2-1";
-	Stack* stack = createStack();
-	char answer[SECOND_TEST_SIZE] = "000";
+	char expression[SECOND_TEST_SIZE + 1] = "2-1";
+	char answer[SECOND_TEST_SIZE + 1] = "";
 	char rightAnswer[SECOND_TEST_SIZE] = "21-";
-	int amountOfBrackets = 0;
-	int result = sortingStation(expression, answer, SECOND_TEST_SIZE, &amountOfBrackets, stack);
-	for (int i = 0; i < SECOND_TEST_SIZE - amountOfBrackets; i++) {
+	int result = sortingStation(expression, answer);
+	for (int i = 0; i < SECOND_TEST_SIZE; i++) {
 		if (answer[i] != rightAnswer[i]) {
 			return false;
 		}
@@ -102,37 +133,20 @@ int main() {
 		return -1;
 	}
 
-	int length = 0;
-	Stack* stack = createStack();
-	printf("Enter the length of your expression: ");
-	scanf_s("%d", &length);
-	char* expression = calloc(length, sizeof(char));
-	if (expression == NULL) {
-		printf("There are problems with memory allocation");
+	char expression[MAX_SIZE] = "";
+	printf("Enter your expression: ");
+	fgets(expression, MAX_SIZE, stdin);
+	char answer[MAX_SIZE] = "";;
+	expression[(int)strlen(expression) - 1] = '\0';
+	int result = sortingStation(expression, answer);
+	if (result == -1) {
+		printf("\nThe expression is wrong.");
+		return -1;
+	} else if (result == -2) {
+		printf("\nThere are problems with memory allocation.");
 		return -1;
 	}
 
-	printf("\nEnter your expression: ");
-	scanf_s("%s", expression, (unsigned)sizeof(expression) * length);
-	char* answer = calloc(length, sizeof(char));
-	if (answer == NULL) {
-		printf("There are problems with memory allocation");
-		free(expression);
-		return -1;
-	}
-
-	int amountOfBrackets = 0;
-	int result = sortingStation(expression, answer, length, &amountOfBrackets, stack);
-	if (result != 0) {
-		deleteStack(stack);
-		free(expression);
-		free(answer);
-		return -1;
-	}
-
-	for (int i = 0; i < (length - amountOfBrackets); i++) {
-		putchar(answer[i]);
-	}
-
+	printf("\nThe answer is: %s", answer);
 	return 0;
 }
