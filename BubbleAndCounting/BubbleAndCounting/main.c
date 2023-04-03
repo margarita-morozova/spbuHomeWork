ae592e41f7b58a881c03143b3c0833f130bdf5ea
Margarita Morozova <s.volland@mail.ru>
2023-04-03 10:51:53 +0300

    Fixed brackets, constants, bubble function is void now, fixed counting function, redone timeCalculation function

--- BubbleAndCounting/BubbleAndCounting/main.c
+++ BubbleAndCounting/BubbleAndCounting/main.c
@@ -2,114 +2,99 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdbool.h>
+#include <string.h>
 
-#define firstTestSize 10
-#define secondTestSize 5
+#define FIRST_TEST_SIZE 10
+#define SECOND_TEST_SIZE 5
 
-int* bubble(int* pointerToArray, int size)
-{
-	for (int i = 0; i < size - 1; i++)
-	{
-		for (int j = 0; j < size - 1 - i; j++)
-		{
-			if (pointerToArray[j] > pointerToArray[j + 1])
-			{
+void bubble(int* pointerToArray, int size) {
+	for (int i = 0; i < size - 1; i++) {
+		for (int j = 0; j < size - 1 - i; j++) {
+			if (pointerToArray[j] > pointerToArray[j + 1]) {
 				int temporaryPlace = pointerToArray[j];
 				pointerToArray[j] = pointerToArray[j + 1];
 				pointerToArray[j + 1] = temporaryPlace;
 			}
 		}
 	}
-
-	return pointerToArray;
 }
 
-int* counting(int* pointerToArray, int size)
-{
+int findMin(int* pointerToArray, int size) {
+	int minimum = 10001;
+	for (int i = 0; i < size; i++) {
+		minimum = min(minimum, pointerToArray[i]);
+	}
+
+	return minimum >= 0 ? 0 : minimum;
+}
+
+void counting(int* pointerToArray, int size) {
+	int minimumInArray = findMin(pointerToArray, size);
 	int extreme = 10000;
 	int* arrayOfNumbers = calloc(extreme, sizeof(int));
 
-	for (int i = 0; i < size; i++)
-	{
-		arrayOfNumbers[pointerToArray[i]]++;
+	for (int i = 0; i < size; i++) {
+		arrayOfNumbers[pointerToArray[i] - minimumInArray]++;
 	}
 
 	int index = 0;
-	while (index <= size)
-	{
-		for (int i = 0; i < extreme; i++)
-		{
-			while (arrayOfNumbers[i] > 0)
-			{
-				pointerToArray[index] = i;
-				arrayOfNumbers[i]--;
+	while (index < size) {
+		for (int i = 0; i < extreme; i++) {
+			while (arrayOfNumbers[i + minimumInArray] > 0) {
+				pointerToArray[index] = i + minimumInArray;
+				arrayOfNumbers[i + minimumInArray]--;
 				index++;
 			}
 		}
 		index++;
 	}
-	free(arrayOfNumbers);
 
-	return pointerToArray;
+	free(arrayOfNumbers);
 }
 
-int* manualFilling(int *pointerToArray, int size)
-{
+int* manualFilling(int *pointerToArray, int size) {
 	printf("Enter the positive elements of your array:\n");
-	for (int i = 0; i < size; i++)
-	{
+	for (int i = 0; i < size; i++) {
 		scanf_s("%d", &pointerToArray[i]);
 	}
 
 	return pointerToArray;
 }
 
-int* randomFilling(int *pointerToArray, int size)
-{
-	for (int i = 0; i < size; i++)
-	{
+int* randomFilling(int *pointerToArray, int size) {
+	for (int i = 0; i < size; i++) {
 		pointerToArray[i] = rand() % 10000;
 	}
 
 	return pointerToArray;
 }
 
-double countingTime()
-{
+void timeCalculation(double* countingTime, double* bubbleTime) {
 	int size = 100000;
 	int* firstArray = (int*)calloc(size, sizeof(int));
+	int* secondArray = (int*)calloc(size, sizeof(int));
 	randomFilling(firstArray, size);
-	clock_t beginning = clock();
+	memcpy(secondArray, firstArray, size);
+	clock_t beginningCounting = clock();
 	counting(firstArray, size);
-	clock_t ending = clock();
+	clock_t endingCounting = clock();
+	clock_t beginningBubble = clock();
+	bubble(secondArray, size);
+	clock_t endingBubble = clock();
 
-	return (double)(ending - beginning) / CLOCKS_PER_SEC;
+	*countingTime = (double)(endingCounting - beginningCounting) / CLOCKS_PER_SEC;
+	*bubbleTime = (double)(endingBubble - beginningBubble) / CLOCKS_PER_SEC;
 }
 
-double bubbleTime()
-{
-	int size = 100000;
-	int* firstArray = (int*)calloc(size, sizeof(int));
-	randomFilling(firstArray, size);
-	clock_t beginning = clock();
-	bubble(firstArray, size);
-	clock_t ending = clock();
+bool firstTest() {
+	int firstTestArray[FIRST_TEST_SIZE] = { 0, 50, 1043, 49, 2, 0, 1, 1, 50, 33 };
+	int secondTestArray[FIRST_TEST_SIZE] = { 50, 2, 1043, 49, 50, 1, 1, 0, 0, 33 };
+	int sortedArray[FIRST_TEST_SIZE] = { 0, 0, 1, 1, 2, 33, 49, 50, 50, 1043 };
+	bubble(firstTestArray, FIRST_TEST_SIZE);
+	counting(secondTestArray, FIRST_TEST_SIZE);
 
-	return (double)(ending - beginning) / CLOCKS_PER_SEC;
-}
-
-bool firstTest()
-{
-	int firstTestArray[firstTestSize] = { 0, 50, 1043, 49, 2, 0, 1, 1, 50, 33 };
-	int secondTestArray[firstTestSize] = { 50, 2, 1043, 49, 50, 1, 1, 0, 0, 33 };
-	int sortedArray[firstTestSize] = { 0, 0, 1, 1, 2, 33, 49, 50, 50, 1043 };
-	bubble(firstTestArray, firstTestSize);
-	counting(secondTestArray, firstTestSize);
-
-	for (int i = 0; i < firstTestSize; i++)
-	{
-		if (firstTestArray[i] != secondTestArray[i] || firstTestArray[i] != sortedArray[i])
-		{
+	for (int i = 0; i < FIRST_TEST_SIZE; i++) {
+		if (firstTestArray[i] != secondTestArray[i] || firstTestArray[i] != sortedArray[i]) {
 			return false;
 		}
 	}
@@ -117,18 +102,15 @@ bool firstTest()
 	return true;
 }
 
-bool secondTest()
-{
-	int firstTestArray[secondTestSize] = { 1000, 5, 5, 1, 1};
-	int secondTestArray[secondTestSize] = { 1, 5, 1, 5, 1000};
-	int sortedArray[secondTestSize] = { 1, 1, 5, 5, 1000 };
-	bubble(firstTestArray, secondTestSize);
-	counting(secondTestArray, secondTestSize);
+bool secondTest() {
+	int firstTestArray[SECOND_TEST_SIZE] = { 1000, 5, 5, 1, 1};
+	int secondTestArray[SECOND_TEST_SIZE] = { 1, 5, 1, 5, 1000};
+	int sortedArray[SECOND_TEST_SIZE] = { 1, 1, 5, 5, 1000 };
+	bubble(firstTestArray, SECOND_TEST_SIZE);
+	counting(secondTestArray, SECOND_TEST_SIZE);
 
-	for (int i = 0; i < secondTestSize; i++)
-	{
-		if (firstTestArray[i] != secondTestArray[i] || firstTestArray[i] != sortedArray[i])
-		{
+	for (int i = 0; i < SECOND_TEST_SIZE; i++) {
+		if (firstTestArray[i] != secondTestArray[i] || firstTestArray[i] != sortedArray[i]) {
 			return false;
 		}
 	}
@@ -136,10 +118,8 @@ bool secondTest()
 	return true;
 }
 
-int main()
-{
-	if (!firstTest() || !secondTest())
-	{
+int main() {
+	if (!firstTest() || !secondTest()) {
 		printf("Tests failed :c");
 		return -1;
 	}
@@ -148,28 +128,28 @@ int main()
 	int size = 0;
 	int firstDecision = 0;
 	int secondDecision = 0;
-	printf("Time that takes to sort the array of 100000 elements using bubble sort: %f\n", bubbleTime());
-	printf("Time that takes to sort the array of 100000 elements using counting sort: %f\n", countingTime());
+	double countingTime = 0;
+	double bubbleTime = 0;
+	timeCalculation(&countingTime, &bubbleTime);
+	printf("Time that takes to sort the array of 100000 elements using bubble sort: %f\n", bubbleTime);
+	printf("Time that takes to sort the array of 100000 elements using counting sort: %f\n", countingTime);
 	printf("Enter the size of your array: ");
 	scanf_s("%d", &size);
 	printf("\nDo you want to enter the whole array or fill it randomly?\nFor manual filling press 0\nFor automatically filling press 1\n");
 	scanf_s("%d", &firstDecision);
 
-	while (firstDecision != 0 && firstDecision != 1)
-	{
+	while (firstDecision != 0 && firstDecision != 1) {
 		printf("Try again\nDo you want to enter the whole array or fill it randomly?\nFor manual filling press 0\nFor automatically filling press 1\n");
 		scanf_s("%d", &firstDecision);
 	}
 
 	int* firstArray = (int*)calloc(size, sizeof(int));
-	if (firstArray == NULL)
-	{
+	if (firstArray == NULL) {
 		printf("\nmemory is not found :c");
 		return -1;
 	}
 
-	if (firstDecision == 0)
-	{
+	if (firstDecision == 0) {
 		manualFilling(firstArray, size);
 	} else {
 		randomFilling(firstArray, size);
@@ -177,22 +157,19 @@ int main()
 
 	printf("If you want to use bubble sort, press 0\nIf you want to use counting sort press 1\n");
 	scanf_s("%d", &secondDecision);
-	while (secondDecision != 0 && secondDecision != 1)
-	{
+	while (secondDecision != 0 && secondDecision != 1) {
 		printf("Try again\nIf you want to use bubble sort, press 0\nIf you want to use counting sort press 1\n");
 		scanf_s("%d", &secondDecision);
 	}
 
-	if (secondDecision == 0)
-	{
+	if (secondDecision == 0) {
 		bubble(firstArray, size);
 	} else {
 		counting(firstArray, size);
 	}
 
 	printf("\nSorted array: ");
-	for (int i = 0; i < size; i++)
-	{
+	for (int i = 0; i < size; i++) {
 		printf("%d ", firstArray[i]);
 	}
 
